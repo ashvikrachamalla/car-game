6d7fc034621ec7db98f27c4d72e45032d1ac114a
ashvikrachamalla <68549074+ashvikrachamalla@users.noreply.github.com>
2020-09-29 19:50:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var database;
+var playerCount = 0;
+var gameState = 0;
+var form, player,game;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+
+}
+
